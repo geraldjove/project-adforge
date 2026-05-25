5dfe8a6a4b4f3be2d657f7e3a0eac6e1c7e727b1
Gerald Jove <geraldkevinjove@gmail.com>
2026-05-26 01:00:23 +0800

    Add GitHub Pages Deployment & Build:

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/project-adforge/",
-});
+  base: command === "build" ? "/project-adforge/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
