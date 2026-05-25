f5b2460534b04b2935dc8dda7bc9bb002c09c35b
Gerald Jove <geraldkevinjove@gmail.com>
2026-05-26 00:47:52 +0800

    Fix GitHub Pages base patch

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/project-adforge/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
