3383ef3be235b2fe50a7ef2440b2be4609039bd2
Gerald Jove <geraldkevinjove@gmail.com>
2026-05-26 00:33:38 +0800

    edited vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/project-adforge/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
